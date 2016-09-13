097306ec3fb7935ba63f9c6a7b472f091642ec6a
alaint16 <atovar@optimeconsulting.com>
2016-09-13 13:30:54 -0400

    routers contacts
    creation of the routers contacts

--- routes/contacts.js
+++ routes/contacts.js
@@ -3,9 +3,37 @@ var router = express.Router(); // get an instance of the express Router
 var Contact = require('../models/contact');
 
 router.route('/')
-.get(function(req, res){
-console.log("it's working")
-res.json({message: 'the contact is working' })
+    .get(function(req, res) {
+        Contact.find(function(err, contacts) {
+            if (err) {
+                res.send(err);
+            } else {
+                res.json({ data: contacts });
+            }
 
-})
+        })
+    })
+    .post(function(req, res) {
+        console.log("it's working")
+        res.json({ message: 'create a new user' })
+
+    })
+
+router.route("/:id")
+    .get(function(req, res) {
+        console.log("it's working")
+        res.json({ message: 'get the user by id' })
+
+    })
+    .put(function(req, res) {
+        console.log("it's working")
+        res.json({ message: 'update one user' })
+    })
+    .delete(function(req, res) {
+        console.log("it's working")
+        res.json({ message: 'delete user' })
+
+    })
+
+
 module.exports = router
