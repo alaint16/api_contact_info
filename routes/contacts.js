2911ac6d454826cc2751c168e74cea1bcab4bbdc
alaint16 <atovar@optimeconsulting.com>
2016-09-22 11:53:31 -0400

    router file updated
    router file updated

--- routes/contacts.js
+++ routes/contacts.js
@@ -2,8 +2,23 @@ var express = require('express');
 var router = express.Router(); // get an instance of the express Router
 var Contact = require('../models/contact');
 
-router.route('/')
+//Middleware
+router.use(function(req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
+    next();
+});
 
+router.route('/')
+// create users
 .post(function(req, res) {
     var contact = new Contact();
     contact.name = req.body.name;
